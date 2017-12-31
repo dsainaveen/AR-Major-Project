89bb20213c04174c6d3fa985026486d52e75876e
D.SaiNaveen <draksharapu.sainaveen@gmail.com>
2017-12-31 18:59:05 +0530

    developed EDC book AR

---
+++ Assets/scripts/buttoncontroller.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class buttoncontroller : MonoBehaviour {
+	//public ArrayList pages = {""};
+	public int pageno=0;
+	public int maxpages = 5;
+	public Renderer rend;
+	void Start() {
+		rend = GetComponent<Renderer> ();
+		Material material = (Material)Resources.Load ("con0") as Material;
+		rend.material = material;
+		//rend.material = con + pageno;
+	}
+
+	public void GotoNextPage()
+	{   
+		if(pageno <= maxpages){
+		Debug.Log ("con" + pageno);
+		Material material = (Material)Resources.Load ("con" + pageno) as Material;
+		/*foreach(Material matt in GetComponent<Renderer>.material)
+		{
+			if(matt.name == "con"+pageno)
+				//do something
+				Debug.Log("found materil"+pageno);
+			}
+			*/
+		rend.material = material;
+		pageno = pageno+1;
+		//Debug.Log("You have clicked next page!");
+		//rend.material = Resources.FindObjectsOfTypeAll (typeof(Material))[pageno];
+		}else{
+			
+		}
+	}
+
+	public void GotoPreviousPage()
+	{ 
+		if (pageno >= 0) {
+			Material material = (Material)Resources.Load ("con" + pageno) as Material;
+			rend.material = material;
+			pageno--;
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ Assets/scripts/videocontroller.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class videocontroller : MonoBehaviour {
+
+
+	//public MovieTexture movTexture;
+	void Start() {
+		//GetComponent<Renderer>().material.mainTexture = movTexture;
+		//movTexture.Play();
+	}
+		
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
